4161a17f0013e8b501642591bb3cc196caca7ef2
Radost Rychtera <radostrychtera@gmail.com>
2018-02-06 17:41:38 +0100

    pushing to heroku

--- application.py
+++ application.py
@@ -36,7 +36,7 @@ app.jinja_env.globals.update(enumerate=enumerate, str=str)
 
 # Flask-SQLAlchemy
 app.config["SQLALCHEMY_TRACK_MODIFICATIONS"] = False
-app.config["SQLALCHEMY_DATABASE_URI"] = "sqlite:///adopter.db"
+app.config["SQLALCHEMY_DATABASE_URI"] = os.environ['DATABASE_URL']
 app.config["SQLALCHEMY_ECHO"] = True
 db = SQLAlchemy(app)
 
